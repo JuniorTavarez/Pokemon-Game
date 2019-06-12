d1af4729099c535946bd73aa9bb883a94ad906a4
Junior <jtavarez8@gmail.com>
2019-06-11 22:25:35 -0400

    almost done pokemon project

--- src/Pokegame.js
+++ src/Pokegame.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Pokedex from './Pokedex'
 
 class Pokegame extends Component {
     static defaultProps = {
@@ -22,11 +23,12 @@ class Pokegame extends Component {
             let randPokemon = hand2.splice(randIdx, 1)[0];
             hand1.push(randPokemon)
         }
-        console.log(hand1)
-        console.log(hand2)
+        let exp1 = hand1.reduce((exp, pokemon) => exp + pokemon.base_experience, 0)
+        let exp2 = hand2.reduce((exp, pokemon) => exp + pokemon.base_experience, 0)
         return (
             <div>
-            <h1>Pokegame!</h1>
+            <Pokedex pokemon={hand1} exp={exp1} isWinner={exp1 > exp2}/>
+            <Pokedex pokemon={hand2} exp={exp2} isWinner={exp2 > exp1}/>
             </div>
         )
     }
