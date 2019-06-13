087f909d55e242429c68c1dc2d5785784831ab30
Junior <jtavarez8@gmail.com>
2019-06-13 05:26:38 -0400

    Updates

---
+++ precache-manifest.ba634644a4574595bfc95d1a5919d536.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c2e85753ea03deecd84cb514c0018cc",
+    "url": "/Pokemon-Game/index.html"
+  },
+  {
+    "revision": "ffbcf24c0846bdaed2b7",
+    "url": "/Pokemon-Game/static/css/main.f7957947.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/Pokemon-Game/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "ffbcf24c0846bdaed2b7",
+    "url": "/Pokemon-Game/static/js/main.aaa8ee76.chunk.js"
+  },
+  {
+    "revision": "2c41fd14e318821d5a1d",
+    "url": "/Pokemon-Game/static/js/runtime~main.c1e85a01.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4164f5d2756723ec54eabeea2a4e74d1.js"
+  "/Pokemon-Game/precache-manifest.ba634644a4574595bfc95d1a5919d536.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Pokemon-Game/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
